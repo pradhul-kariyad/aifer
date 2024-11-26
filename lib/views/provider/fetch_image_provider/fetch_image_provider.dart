5ae06c904ee5b98f476b36eeab0aef8c4627ec2c
unknown <pradhulkariyad@gmail.com>
2024-11-26 15:25:38 +0530

    second commit

--- lib/views/provider/fetch_image_provider/fetch_image_provider.dart
+++ lib/views/provider/fetch_image_provider/fetch_image_provider.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, avoid_print
+// ignore_for_file: file_names, avoid_print, unused_local_variable
 import 'dart:convert';
 import 'dart:developer';
 import 'package:http/http.dart' as http;
@@ -7,19 +7,21 @@ import 'package:flutter/foundation.dart';
 class FetchImageProvider extends ChangeNotifier {
   Future<List<dynamic>?> fetchUnsplashImages() async {
     var apiKey = "qDi5RnVgODf3Jsfhw1cJ7ignoWQhjFKni4ZlfEHjdCo";
+    var id = 679982;
     try {
       final response = await http.get(
         Uri.parse('https://api.unsplash.com/photos/?client_id=$apiKey'),
+        // headers: {"Authorization": "$id $apiKey"}
       );
       if (response.statusCode == 200) {
         log('body : ${response.body}');
         return json.decode(response.body);
       } else {
-        print('Failed to load images');
+        log('Failed to load images');
         return null;
       }
     } catch (e) {
-      print('Error fetching images: $e');
+      log('Error: $e');
       return null;
     }
   }
